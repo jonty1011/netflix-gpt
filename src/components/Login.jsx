545a2e0756267d235675146ef31e1604608b59be
jonty1011 <jonty.suthar@gmail.com>
2025-02-08 22:33:44 +0530

    commit this to current repo

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header"
+
+const Login = () => {
+  return (
+    <div>
+      <Header/>
+      <div>
+      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/638e9299-0637-42d1-ba39-54ade4cf2bf6/web/IN-en-20250203-TRIFECTA-perspective_46eb8857-face-4ea6-b901-dbf22b461369_large.jpg" alt="Background-image" />
+    </div>
+    </div>
+  )
+}
+
+export default Login
